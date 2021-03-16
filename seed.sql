6baada25d23f281c100e342a535ac0690e3a2545
RyRyIT <74007392+RyRyIT@users.noreply.github.com>
2021-03-16 18:12:16 -0400

    Update seed.sql

---
+++ seed.sql
@@ -0,0 +1,20 @@
+USE attendance_db;
+
+INSERT INTO teachers (lastName, firstName) VALUES ("Hawthorne", "Ingrid");
+INSERT INTO teachers (lastName, firstName) VALUES ("Parnell", "Gary");
+INSERT INTO teachers (lastName, firstName) VALUES ("Williams", "Michael");
+INSERT INTO teachers (lastName, firstName) VALUES ("Kennebrew", "Silas");
+INSERT INTO teachers (lastName, firstName) VALUES ("Manningham", "Ivan");
+INSERT INTO teachers (lastName, firstName) VALUES ("Tucci", "David");
+INSERT INTO teachers (lastName, firstName) VALUES ("Winters-Hurst", "Natosha"); 
+
+
+USE attendance_db;
+
+INSERT INTO Classes (className) VALUES ("Biology");
+INSERT INTO Classes (className) VALUES ("ELA");
+INSERT INTO Classes (className) VALUES ("JROTC");
+INSERT INTO Classes (className) VALUES ("US History");
+INSERT INTO Classes (className) VALUES ("Family Living & Mgmt");
+INSERT INTO Classes (className) VALUES ("Algebra");
+INSERT INTO Classes (className) VALUES ("Computer Technology");
