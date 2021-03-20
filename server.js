31c42346247428493f9fe9430031a3a2caf3c0ff
jlw429 <jlw00329@gmail.com>
2021-03-20 14:11:16 -0400

    added small changes most worked was slacked out

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const session = require('express-session');
 
+
 // Requiring passport as we've configured it
 const passport = require('./config/passport');
 
@@ -28,9 +29,31 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring routes
-require('./routes/html-routes.js')(app);
-require('./routes/class_api_routes.js')(app);
+require('./routes/html_routes.js')(app);
+require('./routes/att_api_routes.js')(app);
 require('./routes/student_api_routes.js')(app);
+require('./routes/passport_api_routes')(app);
+
+//SendGrid Email
+
+sgMail.setApiKey(
+  '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'
+  // 'lYcjSIyvTcOYk9JhaOhFSQ.LeJZLwj2-bN-_U_bSlmD832g_oN5u6B6DxH70JA6noc'
+);
+const msg = {
+  to: 'jasonlwest@gmail.com', // Change to your recipient
+  from: 'jlw00329@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg, (err, info) => {
+  if (err) {
+    console.log('Email not Sent');
+  } else {
+    console.log('Your Email was Sent');
+  }
+});
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
