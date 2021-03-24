4d631ba5d11058b9ccc3b8e6d2a3f262f2e0be53
Ryan Lackey <lackeyr13@gmail.com>
2021-03-24 11:04:37 -0400

    testing

--- public/js/student.js
+++ public/js/student.js
@@ -1,151 +1,94 @@
-// Wait for the DOM to completely load before we run our JS
-document.addEventListener('DOMContentLoaded', (e) => {
-  if (e) {
-    console.log('DOM loaded! 🚀');
-  }
+const mysql = require('mysql');
 
-  const StudentContainer = document.querySelector('.student-container');
-  const StudentCategorySelect = document.getElementById('category');
+const connection = mysql.createConnection({
+  host: 'localhost',
 
-  let posts;
+  // Your port; if not 3306
+  port: 3306,
 
-  // Function to grab posts from the database
-  const getStudent = (category) => {
-    let categoryString = category || '';
-    if (categoryString) {
-      categoryString = categoryString.replace(' ', '');
-      categoryString = `category/${categoryString}`;
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: 'MerRy042021!',
+  database: 'attendance_db',
+});
+
+const getStudent = () => {
+  addEventListener('click', (e) => { 
+  console.log('Selecting Students \n');
+  connection.query('SELECT * FROM students', (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+})};
+
+const deleteStudent = () => { 
+  addEventListener('click', (e) => {
+  console.log('Deleting Student..\n');
+  connection.query('DELETE FROM students WHERE ?',
+    {
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} products deleted!\n`);
+      // Call readProducts AFTER the DELETE completes
+      getStudent();
     }
+  );
+})};
 
-    fetch(`/api/student/${categoryString}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+const updateStudent = () => {
+  console.log('Updating Students...\n');
+  const query = connection.query(
+    'UPDATE students SET ? WHERE ?',
+    [
+      {
+        teacher: 'Michael Williams',
       },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Student retrieved:', data);
-        posts = data;
-
-        if (!student.length) {
-          displayEmpty();
-        } else {
-          initializeRows();
-        }
-      })
-      .catch((error) => console.error('Error:', error));
-  };
-
-  // Function to make DELETE request for a post
-  const deleteStudent = (id) => {
-    fetch(`/api/student/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
+      {
+        name: 'Allen, Delaphonta',
       },
-    }).then(() => getStudent(StudentCategorySelect.value));
-  };
+    ],
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} Student updated!\n`);
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteStudent();
+    }
+  );
 
-  // Getting initial list of posts
-  getStudent();
-
-  // Function to help construct the post HTML content inside blogContainer
-  const initializeRows = () => {
-    blogContainer.innerHTML = '';
-    const postsToAdd = [];
-
-    posts.forEach((post) => postsToAdd.push(createNewRow(post)));
-    postsToAdd.forEach((post) => blogContainer.appendChild(post));
-  };
-
-  const createNewRow = (post) => {
-    // Postcard div
-    const newPostCard = document.createElement('div');
-    newPostCard.classList.add('card');
-
-    // Heading
-    const newPostCardHeading = document.createElement('div');
-    newPostCardHeading.classList.add('card-header');
-
-    // Delete button
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'x';
-    deleteBtn.classList.add('delete', 'btn', 'btn-danger');
-    deleteBtn.addEventListener('click', handlePostDelete);
-
-    // Edit button
-    const editBtn = document.createElement('button');
-    editBtn.textContent = 'EDIT';
-    editBtn.classList.add('delete', 'btn', 'btn-danger');
-    editBtn.addEventListener('click', handlePostEdit);
-
-    // New post info
-    const newPostTitle = document.createElement('h2');
-    const newPostDate = document.createElement('small');
-
-    // New post category
-    const newPostCategory = document.createElement('h5');
-    newPostCategory.textContent = post.category;
-    newPostCategory.style.float = 'right';
-    newPostCategory.style.fontWeight = '700';
-    newPostCategory.style.marginTop = '-15px';
-
-    // New post card body
-    const newPostCardBody = document.createElement('div');
-    newPostCardBody.classList.add('card-body');
-
-    // New Post
-    const newPostBody = document.createElement('p');
-    newPostTitle.textContent = post.title;
-    newPostBody.textContent = post.body;
-
-    const formattedDate = new Date(post.createdAt).toLocaleDateString();
-    newPostDate.textContent = ` (${formattedDate})`;
-
-    newPostTitle.appendChild(newPostDate);
-    newPostCardHeading.appendChild(deleteBtn);
-    newPostCardHeading.appendChild(editBtn);
-    newPostCardHeading.appendChild(newPostTitle);
-    newPostCardHeading.appendChild(newPostCategory);
-    newPostCardBody.appendChild(newPostBody);
-    newPostCard.appendChild(newPostCardHeading);
-    newPostCard.appendChild(newPostCardBody);
-    newPostCard.setAttribute('data-post', JSON.stringify(post));
-
-    return newPostCard;
-  };
-/*
-  const handlePostDelete = (e) => {
-    const currentPost = JSON.parse(
-      e.target.parentElement.parentElement.dataset.post
-    );
-    console.log('handlePostDelete -> currentPost', currentPost);
-    deletePost(currentPost.id);
-  };
-
-  const handlePostEdit = (e) => {
-    const currentPost = JSON.parse(
-      e.target.parentElement.parentElement.dataset.post
-    );
-    console.log('handlePostDelete -> currentPost', currentPost);
-    window.location.href = `/cms?post_id=${currentPost.id}`;
-  };
-
-  const displayEmpty = () => {
-    blogContainer.innerHTML = '';
-    const messageH2 = document.createElement('h4');
-    messageH2.style.textAlign = 'center';
-    messageH2.style.marginTop = '50px';
-    messageH2.innerHTML = `No posts yet for this category. <br>Click <a href="/cms">here</a> to make a new post.`;
-    blogContainer.appendChild(messageH2);
-  };
-
-  const handleCategoryChange = (e) => {
-    const newPostCategory = e.target.value;
-    console.log('handleCategoryChange -> newPostCategory', newPostCategory);
-    getPosts(newPostCategory.toLowerCase());
-  };
-  postCategorySelect.addEventListener('change', handleCategoryChange); 
+  // logs the actual query being run
+  console.log(query.sql);
 };
-*/
+
+const createStudent = () => {
+  console.log('Insert New student...\n');
+  const query = connection.query(
+    'INSERT INTO students SET ?',
+    {
+      student_name: 'Ryan Lackey',
+      teacher_main: 'Gary Parnell',
+      teacher_email: 'jlw00329@gmail.com',
+    },
+    (err, res) => {
+      if (err) throw err;
+      console.log(`${res.affectedRows} Student inserted!\n`);
+      // Call updateProduct AFTER the INSERT completes
+      updateSudent();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+};
+
+// Connect to the DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  createStudent();
+});
+
